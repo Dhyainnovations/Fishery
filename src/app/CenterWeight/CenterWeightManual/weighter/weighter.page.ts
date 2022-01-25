4c861bbff367f1c922f3505d6b110f7bff2cdff2
Dhya Innovations <dhyainnovations@Dhyas-Mac-mini.local>
2022-01-25 13:56:12 +0530

    login api

--- src/app/CenterWeight/CenterWeightManual/weighter/weighter.page.ts
+++ src/app/CenterWeight/CenterWeightManual/weighter/weighter.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpService } from '../weighter/./../../../shared/http.service';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-weighter',
@@ -7,9 +10,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WeighterPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private activatedRoute: ActivatedRoute, private http: HttpService, route: ActivatedRoute,) {
+    route.params.subscribe(val => {
+      this.getCategoryList()
+      this.getTypeList()
+      this.getLocationList()
+    });
+
+   }
 
   ngOnInit() {
   }
   currentDate = new Date();
+
+  categorylist:any = []
+  locationlist:any = []
+  typelist:any = []
+
+  getCategoryList(){
+    this.http.get('/list_category', ).subscribe((response: any) => {
+      this.categorylist = response.records;
+      console.log(response);
+
+    }, (error: any) => {
+      console.log(error);
+    }
+    );
+
+  }
+  
+  getTypeList(){
+    this.http.get('/list_type', ).subscribe((response: any) => {
+      this.typelist = response.records;
+      console.log(response);
+
+    }, (error: any) => {
+      console.log(error);
+    }
+    );
+
+  }
+
+  getLocationList(){
+    this.http.get('/list_location', ).subscribe((response: any) => {
+      this.locationlist = response.records;
+      console.log(response);
+
+    }, (error: any) => {
+      console.log(error);
+    }
+    );
+
+  }
+
+
 }
--- src/app/login-page/login-page.page.ts
+++ src/app/login-page/login-page.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpService } from '../shared/http.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login-page',
@@ -8,11 +10,158 @@ import { Router } from '@angular/router';
 })
 export class LoginPagePage implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private http: HttpService) { }
 
   ngOnInit() {
+
   }
-  NavigateToNExtPage(){
-    this.router.navigate(['/centerweight-auto-dashboard'])
+
+  username: any;
+  password: any;
+
+  orgid: any;
+  userName: any;
+  logintype: any;
+  permission: any;
+
+  LocalOrgid: any = (localStorage.getItem("orgid"));
+  LocaluserName: any = (localStorage.getItem("Fishery-username"));
+  Locallogintype: any = (localStorage.getItem("logintype"));
+  Localpermission: any = (localStorage.getItem("permission"));
+
+  login() {
+
+    const Data = {
+      id: this.username,
+      password: this.password
+    }
+
+
+    this.http.post('/login', Data).subscribe((response: any) => {
+      console.log(response);
+
+      if (response.success == "true") {
+        this.orgid = response.orgid,
+        this.username =  response.id,
+        this.logintype = response.loginType,
+        this.permission = response.permission,
+        
+        localStorage.setItem("orgid",this.orgid)
+        localStorage.setItem("Fishery-username",this.username)
+        localStorage.setItem("logintype",this.logintype)
+        localStorage.setItem("permission",this.permission)
+
+        if (response.success == "true") {
+          const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 1000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.addEventListener('mouseenter', Swal.stopTimer)
+              toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+          })
+
+          Toast.fire({
+            icon: 'success',
+            title: 'Signed in successfully'
+          })
+
+
+          console.log(this.Locallogintype);
+          console.log(this.Localpermission);
+          
+          
+         
+
+          //----------- Login type checking --------------//
+
+          if (this.Locallogintype == "ROLE_WSHO") {
+
+            //---------- Auto or Manual Checking -----------//
+
+            if (this.Localpermission == "MANUAL") {
+              this.router.navigate(['/centerweight-manual-weighter'])
+            }
+
+            if (this.Localpermission == "AUTO") {
+              this.router.navigate(['/centerweight-auto-dashboard'])
+            }
+
+          }
+
+          if (this.Locallogintype == "ROLE_LOCALSALE") {
+
+            //---------- Auto or Manual Checking -----------//
+
+            if (this.Localpermission == "MANUAL") {
+              this.router.navigate(['/BillerManualdashboard'])
+            }
+
+            // if (this.userdetails.permission == "AUTO") {
+            //   this.router.navigate(['/centerweight-auto-dashboard'])
+            // }
+          }
+
+
+
+
+
+          // if (this.userdetails.logintype == "ROLE_ADMIN") {
+          //   this.router.navigate(['/admin'])
+          // }
+
+          // if (this.userdetails.logintype == "ROLE_WSHO") {
+          //   this.router.navigate(['/admin'])
+          // }
+
+          // if (this.userdetails.logintype == "ROLE_LOCALSALE") {
+
+          // }
+
+          // if (this.userdetails.logintype == "ROLE_MERCHANT") {
+
+          //   //---------- Auto or Manual Checking -----------//
+
+          //   if (this.userdetails.permission == "MANUAL") {
+          //     this.router.navigate(['/Merchantweight-manual-dashboard'])
+          //   }
+
+          //   if (this.userdetails.permission == "AUTO") {
+          //     this.router.navigate(['/Merchantweight-manual-dashboard'])
+          //   }
+          // }
+
+
+
+        }
+
+
+      } else {
+        const Toast = Swal.mixin({
+          toast: true,
+          position: 'top-end',
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+          }
+        })
+
+        Toast.fire({
+          icon: 'error',
+          title: 'Please enter a valid email (or) password'
+        })
+      }
+    }, (error: any) => {
+      console.log(error);
+    }
+    );
+
+
   }
 }
