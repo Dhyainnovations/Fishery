239bbbd8e616a066164843e40f7c2e99bd6f6eb7
Dhya Innovations <dhyainnovations@Dhyas-Mac-mini.local>
2022-01-31 19:19:49 +0530

    v2 debug/GK

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.fishery.app',
   appName: 'Fishery',
   webDir: 'www',
   bundledWebRuntime: false
